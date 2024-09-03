34d7e03c9cbf3418a0979d41e398c12fa4536d78
prajwalk1931 <rvit21bis018.rvitm@rvei.edu.in>
2024-09-03 21:39:58 +0000

    variable done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=1332
+let accountEmial="hc@gmial.com"
+var accountPassword="1234"
+accountCity="jaipur"
+let accountState;
+
+accountEmial="hcw@gmial.com"
+accountPassword="12345"
+accountCity="jaipuar"
+/*
+if we wont initilze variable in js it takes undefined value
+*/
+console.table([accountEmial,accountPassword,accountCity,accountState]);
+
+
